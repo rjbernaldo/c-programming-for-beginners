292b7da55f737806d181b58c6aaa089cdee7fdee
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-02 19:01:30 +0800

    Add flush input routine

--- 04_GetInput/src/main.c
+++ 04_GetInput/src/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+void flush_input() {
+  int ch;
+  while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
 void get_input_with_fgets() {
   char firstname[5];
   char lastname[5];
 
   printf("Enter your firstname: ");
   fgets(firstname, 5, stdin);
+  flush_input();
 
   printf("Enter your lastname: ");
   fgets(lastname, 5, stdin);
+  flush_input();
 
   printf("Hello, %s, %s\n", firstname, lastname);
 }
