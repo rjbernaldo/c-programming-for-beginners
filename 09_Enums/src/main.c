697890e95ad5ef37277b5eb4b2b638545c89a2d3
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-14 20:01:01 +0800

    Add enums

---
+++ 09_Enums/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum suits {
+  Hearts, Diamonds, Clubs, Spades
+};
+
+void showcard(enum suits card) {
+  if (card == Hearts || card == Diamonds) {
+    printf("This card is red (in suit #%d).\n", card);
+  } else {
+    printf("This card is black (in suit #%d).\n", card);
+  }
+}
+
+int main(int argc, char **argv) {
+  enum suits playingcard;
+  playingcard = Diamonds;
+  showcard(playingcard);
+}
